7af29f5b390b6b71b6eefe5653bf7b64eae112dd
zottwickel <scott.g.mckell@gmail.com>
2019-10-12 21:05:35 -0600

    complete exercise

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import List from './List';
+import './App.css';
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends React.Component {
+  static defaultProps = {
+    store : {
+      lists: [],
+      allCards: {}
+    }
+  };
+  render () {
+    const { store } = this.props
+    const lists = store.lists.map((list, i) => {
+      const listCards = list.cardIds.map(id => {
+        return store.allCards[id]
+      });
+        return (<List key={i} header={list.header} cards={listCards} />)
+    });
+    return (
+      <main className='App'>
+        <header className='App-header'>
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className='App-list'>
+          {lists}
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
---
+++ src/List.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Card from './Card';
+import './List.css';
+
+function List(props) {
+    const cards = props.cards.map( (card) => {
+        return (
+            <Card key={card.id} title={card.title} content={card.content} />
+        )
+    });
+    return (
+        <section className='List'>
+            <header className='List-header'>
+                <h2>{props.header}</h2>
+            </header>
+            <div className='List-cards'>
+                {cards}
+                <button type='button' className='List-add-button'>
+                    + Add Random Card
+                </button>
+            </div>
+        </section>
+    )
+}
+
+export default List;
